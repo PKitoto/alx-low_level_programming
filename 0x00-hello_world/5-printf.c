c8febe4a0b6059f9332b6406237b377a7a238a84
PKitoto <paulokitoto@gmail.com>
2022-04-07 12:05:30 -0700

    Completed analogy -updated

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 
 /**
- * main - Program entry point
+ * main - Prints a text
  *
- * Return: Always 0 (Success) 
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
