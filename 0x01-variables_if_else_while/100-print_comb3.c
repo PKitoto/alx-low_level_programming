220054a1ab8beb04687824f1fb0f32939092cfa6
PKitoto <paulokitoto@gmail.com>
2022-04-08 13:51:17 -0700

    100-print_comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - initialize the code
+ *
+ * Return: zero means successful
+ */
+int main(void)
+{
+	int n = 48;
+	int m = 49;
+	int c = 49;
+
+	while (n < 58)
+	{
+	while (m < 58)
+	{
+	putchar(n);
+	putchar(m);
+	if ((n != 56) || (m != 57))
+	{
+	putchar(44);
+	putchar(32);
+	}
+	m++;
+	}
+	c++;
+	m = c;
+	n++;
+	}
+	putchar('\n');
+	return (0);	
+}
