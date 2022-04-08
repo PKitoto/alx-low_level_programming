84c1cec2908f76343b1490544e8cbb4482607f17
PKitoto <paulokitoto@gmail.com>
2022-04-08 09:13:23 -0700

    print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * Main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int p = 97;
+
+	while (p < 123)
+	{
+	putchar(p);
+	p++;
+	}
+	putchar('\n');
+	return (0);
+}	
